85b74c246ce2f179ecb94dd3f0feb7dcdbeb2ec0
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-09-29 20:09:36 +0000

    updates

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,33 +1,7 @@
 export default function createIteratorObject(report) {
-  const employees = report.allEmployees;
-
-  const departments = Object.keys(employees);
-
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    next() {
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-
-      const departmentName = departments[currentDepartmentIndex];
-      const employeeList = employees[departmentName];
-
-      if (currentEmployeeIndex >= employeeList.length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-        return this.next();
-      }
-
-      const employee = employeeList[currentEmployeeIndex];
-      currentEmployeeIndex++;
-
-      return { value: employee, done: false };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
+  const nArr = [];
+  for (const arr of Object.values(report)) {
+    nArr.push(arr);
+  }
+  return nArr;
 }
