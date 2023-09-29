236396798cc61646301e16efc84037a8b6105f76
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-09-29 21:05:28 +0000

    updates

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency class');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    } else {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    }
+  }
+}
