d84e78e62043aac6a20843d3d7230febcbf491c8
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-09-29 20:41:08 +0000

    updates

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const result = [];
-  
-  for (const department in reportWithIterator.allEmployees) {
-    const employees = reportWithIterator.allEmployees[department];
-    
-    for (const employee of employees) {
-      result.push(employee);
-    }
-  }
-
-  return result.join(' | ');
+  const { allEmployees } = reportWithIterator;
+  const employeeLists = Object.values(allEmployees);
+  const flattenedEmployees = [].concat(...employeeLists);
+  return flattenedEmployees.join(' | ');
 }
